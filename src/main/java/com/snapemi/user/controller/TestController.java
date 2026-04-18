d594389649853737c535754e36147a25e7d9a471
Vinay Singh <143172382+VinaySingh3119@users.noreply.github.com>
2026-04-19 02:38:16 +0530

    Update TestController.java

--- src/main/java/com/snapemi/user/controller/TestController.java
+++ src/main/java/com/snapemi/user/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class TestController {
 
-    @GetMapping("/test")
+   
     public String test() {
         return "Backend is working";
     }
